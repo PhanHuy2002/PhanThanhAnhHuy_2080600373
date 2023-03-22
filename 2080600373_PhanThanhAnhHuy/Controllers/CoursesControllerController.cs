453acc6f9f63948433ee192c004761aedcc39085
PhanHuy2002 <huypk22@gmail.com>
2023-03-22 16:10:13 +0700

    BigSchool Buổi 2

--- 2080600373_PhanThanhAnhHuy/Controllers/CoursesControllerController.cs
+++ 2080600373_PhanThanhAnhHuy/Controllers/CoursesControllerController.cs
@@ -1,5 +1,6 @@
 ﻿using _2080600373_PhanThanhAnhHuy.Models;
 using _2080600373_PhanThanhAnhHuy.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace _2080600373_PhanThanhAnhHuy.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: CoursesController
+        [Authorize]
         public ActionResult Create()
         {
             var viewModels = new CourseViewModel
@@ -23,6 +25,27 @@ namespace _2080600373_PhanThanhAnhHuy.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModels);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course()
+            {
+                LecturerId = User.Identity.GetUserId(),
+                Datetime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place,
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- 2080600373_PhanThanhAnhHuy/Models/AccountViewModels.cs
+++ 2080600373_PhanThanhAnhHuy/Models/AccountViewModels.cs
@@ -79,6 +79,9 @@ namespace _2080600373_PhanThanhAnhHuy.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
